1b9a540c6c684e17b097064e1d5a4978e30ab939
Alex Sieke <asieke@gmail.com>
2022-01-28 14:08:37 -0500

    updated fetching scripts

--- database/fetch.js
+++ database/fetch.js
@@ -1,13 +1,40 @@
+/******************************************
+ * READS recipe IDS from rawdata/ids.csv
+ * Gets the next LIMIT recipes
+ * Saves them to rawdata/max_returned_id.json
+ ******************************************/
+
 const fs = require('fs/promises');
 const axios = require('axios');
 
-const URL =
-  'https://api.spoonacular.com/recipes/random?apiKey=64c72029d6874041844acb68d747458b&number=100&limitLicense=false';
+const LIMIT = 25;
 
 const main = async () => {
+  //GET THE MAX ID THAT HAS ALREADY BEEN SAVED
+  let ids = await fs.readdir(__dirname + '/rawdata/json');
+  ids = ids.map((x) => parseInt(x.substr(0, x.indexOf('.'))));
+  ids.sort((a, b) => b - a);
+  const START = ids[0] || 0;
+
+  //BUILD ARRAY OF THE NEXT [LIMIT] TO FETCH
+  let fetch = [];
+  let stream = await fs.readFile(__dirname + '/rawdata/ids.csv', 'utf8');
+  let data = stream.split('\n').map((x) => parseInt(x));
+  data.forEach((id) => {
+    if (fetch.length < LIMIT && id > START) {
+      fetch.push(id);
+    }
+  });
+
+  //FETCH AND SAVE
+  const URL =
+    'https://api.spoonacular.com/recipes/informationBulk?apiKey=0122f895d78848d690b366c3d0eb7925&ids=' +
+    fetch.join(',');
+
+  const FILE = __dirname + '/rawdata/json/' + fetch[fetch.length - 1] + '.json';
   let res = await axios.get(URL);
-  let path = __dirname + '/rawdata/' + Date.now() + '.json';
-  await fs.writeFile(path, JSON.stringify(res.data));
+  console.log(res.headers['x-api-quota-used']);
+  await fs.writeFile(FILE, JSON.stringify(res.data));
 };
 
 main();
--- database/stats.js
+++ database/stats.js
@@ -1,19 +1,23 @@
 const fs = require('fs/promises');
 
 const main = async () => {
-  let files = await fs.readdir(__dirname + '/rawdata');
+  let files = await fs.readdir(__dirname + '/rawdata/json');
+
+  files.sort(
+    (a, b) => parseInt(a.substr(0, a.indexOf('.'))) - parseInt(b.substr(0, b.indexOf('.')))
+  );
 
   let obj = {};
 
   for (let i = 0; i < files.length; i++) {
-    let data = await fs.readFile(__dirname + '/rawdata/' + files[i]);
+    let data = await fs.readFile(__dirname + '/rawdata/json/' + files[i]);
     let json = JSON.parse(data);
-    for (let r = 0; r < json.recipes.length; r++) {
-      obj[json.recipes[r].id] = 1;
+    for (let r = 0; r < json.length; r++) {
+      let { id, title, image, analyzedInstructions } = json[r];
+      let steps = analyzedInstructions.map((x) => x.steps.length);
+      console.log([id, title, image, steps].join('\t'));
     }
   }
-
-  console.log(Object.keys(obj).length);
 };
 
 main();
