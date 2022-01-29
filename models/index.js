8be53a734e2d82ea4f7bdd56c906414878884100
Alex Sieke <asieke@gmail.com>
2022-01-29 16:22:54 -0500

    logging

--- models/index.js
+++ models/index.js
@@ -4,4 +4,7 @@ const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 const { Pool } = require('pg');
 const pool = new Pool();
 
+console.log('CONNECTING TO DATABASE>>>>>>>>>>');
+console.log(process.env);
+
 module.exports = { pool };
