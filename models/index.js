26bbc6cb62cce732246793d3a5f27261c8adf0e1
Alex Sieke <asieke@gmail.com>
2022-01-29 16:47:12 -0500

    added production db conn

--- models/index.js
+++ models/index.js
@@ -2,9 +2,21 @@ require('dotenv').config();
 
 const { PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
 const { Pool } = require('pg');
-const pool = new Pool();
 
-console.log('CONNECTING TO DATABASE>>>>>>>>>>');
-console.log(process.env);
+let pool;
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log('PRODUCTION');
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      require: true,
+      rejectUnauthorized: true,
+    },
+  });
+} else {
+  console.log('DEV');
+  pool = new Pool();
+}
 
 module.exports = { pool };
