584d80a50449ac259006560dc231134dfbb2c453
Alex Sieke <asieke@gmail.com>
2022-01-28 19:06:34 -0500

    finished db schema

--- database/index.js
+++ database/index.js
@@ -10,36 +10,71 @@ const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
 });
 
 const db = {
-  Recipe: sequelize.define(
-    'recipe',
+  Tag: sequelize.define(
+    'tag',
     {
-      recipe_id: Sequelize.INTEGER,
-      title: Sequelize.STRING,
-      summary: Sequelize.TEXT,
-      readyInMinutes: Sequelize.INTEGER,
-      servings: Sequelize.INTEGER,
-      image: Sequelize.STRING,
-      pricePerServing: Sequelize.FLOAT,
-      sourceName: Sequelize.STRING,
-      sourceUrl: Sequelize.STRING,
-      ingredients: Sequelize.JSON,
-      instructionSections: Sequelize.INTEGER,
-      instructions: Sequelize.JSON,
-      cuisines: Sequelize.JSON,
-      dishTypes: Sequelize.JSON,
-      diets: Sequelize.JSON,
+      name: Sequelize.STRING,
+      category: Sequelize.STRING,
+      frequency: Sequelize.INTEGER,
     },
     {
-      indexes: [
-        {
-          unique: true,
-          fields: ['recipe_id'],
-        },
-      ],
+      indexes: [{ unique: true, fields: ['name'] }],
     }
   ),
+  RecipesTag: sequelize.define(
+    'recipes_tag',
+    {
+      recipe_id: Sequelize.INTEGER,
+      tag_id: Sequelize.INTEGER,
+    },
+    {
+      indexes: [{ fields: ['recipe_id'] }, { fields: ['tag_id'] }],
+    }
+  ),
+  Ingredient: sequelize.define('ingredient', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    name: Sequelize.STRING,
+    category: Sequelize.STRING,
+    frequency: Sequelize.INTEGER,
+  }),
+  RecipesIngredient: sequelize.define(
+    'recipes_ingredient',
+    {
+      recipe_id: Sequelize.INTEGER,
+      ingredient_id: Sequelize.INTEGER,
+    },
+    {
+      indexes: [{ fields: ['recipe_id'] }, { fields: ['ingredient_id'] }],
+    }
+  ),
+  Recipe: sequelize.define('recipe', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+    },
+    title: Sequelize.STRING,
+    aggregateLikes: Sequelize.INTEGER,
+    summary: Sequelize.TEXT,
+    readyInMinutes: Sequelize.INTEGER,
+    servings: Sequelize.INTEGER,
+    image: Sequelize.STRING,
+    pricePerServing: Sequelize.FLOAT,
+    sourceName: Sequelize.STRING,
+    sourceUrl: Sequelize.STRING,
+    ingredients: Sequelize.JSON,
+    instructionSections: Sequelize.INTEGER,
+    instructions: Sequelize.JSON,
+    tags: Sequelize.JSON,
+  }),
   initialize: async () => {
     await db.Recipe.sync({ force: true });
+    await db.Ingredient.sync({ force: true });
+    await db.RecipesIngredient.sync({ force: true });
+    await db.Tag.sync({ force: true });
+    await db.RecipesTag.sync({ force: true });
   },
   close: async () => {
     await sequelize.close();
--- database/initialize.js
+++ database/initialize.js
@@ -1,44 +1,149 @@
 const fs = require('fs/promises');
+const { cuisines } = require('../server/data.js');
 const { db } = require('./index.js');
 
+let INGREDIENTS = {};
+let TAGS = {};
+
 const main = async () => {
-  await db.Recipe.sync({ force: true });
+  // /**********************
+  //  * INITIALIZE DB
+  //  ***********************/
+  // await db.initialize();
 
-  let files = await fs.readdir(__dirname + '/rawdata/json');
+  // /**********************
+  //  * POPULATE RECIPES TABLE
+  //  ***********************/
+  // console.log('POPULATING RECIPES...');
 
-  for (let i = 0; i < files.length; i++) {
-    console.log(i, 'of', files.length, 'reading', files[i]);
-    let data = await fs.readFile(__dirname + '/rawdata/json/' + files[i]);
-    let json = JSON.parse(data);
+  // let files = await fs.readdir(__dirname + '/rawdata/json');
 
-    for (let r = 0; r < json.length; r++) {
-      let rec = json[r];
+  // for (let i = 0; i < files.length; i++) {
+  //   let data = await fs.readFile(__dirname + '/rawdata/json/' + files[i]);
+  //   let json = JSON.parse(data);
 
-      let recipe = {
-        recipe_id: rec.id,
-        title: rec.title,
-        summary: rec.summary,
-        readyInMinutes: rec.readyInMinutes,
-        servings: rec.servings,
-        image: rec.image,
-        pricePerServing: rec.pricePerServing,
-        sourceName: rec.sourceName,
-        sourceUrl: rec.sourceUrl,
-        ingredients: rec.extendedIngredients.map((ing) => {
-          return { name: ing.name, original: ing.original };
-        }),
-        instructionSections: rec.analyzedInstructions.length,
-        instructions: rec.analyzedInstructions.map((x) => {
-          return { name: x.name, steps: x.steps.map((s) => s.step) };
-        }),
-        cuisines: rec.cuisines,
-        dishTypes: rec.dishTypes,
-        diets: rec.diets,
-      };
+  //   for (let r = 0; r < json.length; r++) {
+  //     let rec = json[r];
 
-      await db.Recipe.create(recipe);
+  //     //Loop through ingredients array and build table
+  //     rec.extendedIngredients.forEach((item) => {
+  //       //if it already exists
+  //       if (INGREDIENTS[item.id]) {
+  //         INGREDIENTS[item.id].frequency++;
+  //       } else {
+  //         INGREDIENTS[item.id] = {
+  //           id: item.id,
+  //           name: item.nameClean || item.name,
+  //           category: item.aisle,
+  //           frequency: 1,
+  //         };
+  //       }
+  //     });
+
+  //     //Loop through tags
+  //     [
+  //       [rec.cuisines, 'cuisines'],
+  //       [rec.dishTypes, 'dish'],
+  //       [rec.diets, 'diets'],
+  //     ].forEach((arr) => {
+  //       arr[0].forEach((item) => {
+  //         if (TAGS[item]) {
+  //           TAGS[item].frequency++;
+  //         } else {
+  //           TAGS[item] = {
+  //             name: item,
+  //             frequency: 1,
+  //             category: arr[1],
+  //           };
+  //         }
+  //       });
+  //     });
+
+  //     let recipe = {
+  //       id: rec.id,
+  //       title: rec.title,
+  //       summary: rec.summary,
+  //       readyInMinutes: rec.readyInMinutes,
+  //       aggregateLikes: rec.aggregateLikes,
+  //       servings: rec.servings,
+  //       image: rec.image,
+  //       pricePerServing: rec.pricePerServing,
+  //       sourceName: rec.sourceName,
+  //       sourceUrl: rec.sourceUrl,
+  //       ingredients: rec.extendedIngredients.map((ing) => {
+  //         return { ingredient_id: ing.id, name: ing.nameClean || ing.name, original: ing.original };
+  //       }),
+  //       instructionSections: rec.analyzedInstructions.length,
+  //       instructions: rec.analyzedInstructions.map((x) => {
+  //         return { name: x.name, steps: x.steps.map((s) => s.step) };
+  //       }),
+  //       tags: [...rec.cuisines, ...rec.dishTypes, ...rec.diets],
+  //     };
+
+  //     await db.Recipe.create(recipe);
+  //   }
+  // }
+
+  // /**********************
+  //  * POPULATE INGREDIENTS TABLE
+  //  ***********************/
+  // console.log('POPULATING INGREDIENTS...');
+
+  // let values = Object.values(INGREDIENTS);
+
+  // for (let i = 0; i < values.length; i++) {
+  //   if (!values[i].id) continue;
+  //   await db.Ingredient.create(values[i]);
+  // }
+
+  // /**********************
+  //  * POPULATE RECIPES_INGREDIENTS TABLE
+  //  ***********************/
+  // console.log('POPULATING RECIPES-INGREDIENTS...');
+
+  // let data = await db.Recipe.findAll();
+
+  // for (let i = 0; i < data.length; i++) {
+  //   let ingredients = data[i].ingredients;
+  //   for (let j = 0; j < data[i].ingredients.length; j++) {
+  //     let row = { recipe_id: data[i].id, ingredient_id: data[i].ingredients[j].ingredient_id };
+  //     await db.RecipesIngredient.create(row);
+  //   }
+  // }
+
+  // /**********************
+  //  * POPULATE TAGS
+  //  ***********************/
+  // console.log('POPULATING TAGS...');
+
+  // let tags = Object.values(TAGS);
+  // for (let i = 0; i < tags.length; i++) {
+  //   await db.Tag.create(tags[i]);
+  // }
+
+  /**********************
+   * POPULATE RECIPES_TAGS
+   ***********************/
+  console.log('POPULATING TAGS...');
+
+  const populateTags = async () => {
+    let temp = await db.Tag.findAll();
+    let tag_lookup = {};
+    temp.forEach((x) => {
+      tag_lookup[x.name] = parseInt(x.id);
+    });
+
+    let data = await db.Recipe.findAll();
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].tags.length; j++) {
+        let row = { recipe_id: data[i].id, tag_id: tag_lookup[data[i].tags[j]] };
+        await db.RecipesTag.create(row);
+      }
     }
-  }
+  };
+
+  await populateTags();
 
   db.close();
 };
