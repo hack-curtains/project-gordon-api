5022b48c20f94c2157baa9df4bdb1499e8b7529c
Alex Sieke <asieke@gmail.com>
2022-01-30 18:00:17 -0500

    refactored recipes

--- models/recipes.js
+++ models/recipes.js
@@ -4,36 +4,136 @@ const { pool } = require('./index.js');
 const ABBREVIATED_COLUMNS =
   'id, title, image, servings, price, likes, summary, tags, ingredients, time';
 
+//Ensure that all the query parameters are validated
+const getQueryParamErrors = (obj) => {
+  let e = { message: 'there was an issue with your query parameters', errors: {} };
+
+  if (obj.page <= 0 || !Number.isInteger(parseInt(obj.page))) {
+    e.errors.page = { value: obj.page, message: 'must be a positive integer' };
+  }
+
+  if (obj.count <= 0 || !Number.isInteger(parseInt(obj.count))) {
+    e.errors.count = { value: obj.count, message: 'must be a positive integer' };
+  }
+
+  if (!['default', 'likes', 'price'].includes(obj.sort.toLowerCase())) {
+    e.errors.sort = { value: obj.sort, message: 'must be one of: default|likes|price' };
+  }
+
+  if (!['asc', 'desc'].includes(obj.direction.toLowerCase())) {
+    e.errors.direction = { value: obj.direction, message: 'must be one of: asc|desc' };
+  }
+
+  obj.tag_ids.forEach((id) => {
+    if (id <= 0 || !Number.isInteger(id)) {
+      e.errors.tag_ids = { value: obj.tag_ids, message: 'all tag_ids must be positive integers' };
+    }
+  });
+
+  obj.ingredient_ids.forEach((id) => {
+    if (id <= 0 || !Number.isInteger(id)) {
+      e.errors.ingredient_ids = {
+        value: obj.ingredient_ids,
+        message: 'all ingredient_ids must be positive integers',
+      };
+    }
+  });
+
+  return e;
+};
+
+//Get the WHERE query
+
+const getWhereSQL = ({ include, tag_ids, ingredient_ids, query }) => {
+  let chunks = [];
+
+  let operator = include === true ? '@>' : '&&';
+
+  if (tag_ids.length > 0) {
+    chunks.push(`(tag_ids ${operator} Array[${tag_ids.join(',')}]::integer[])`);
+  }
+
+  if (ingredient_ids.length > 0) {
+    chunks.push(`(ingredient_ids ${operator} ARRAY[${ingredient_ids.join(',')}]::INTEGER[])`);
+  }
+
+  if (query.length > 0) {
+    chunks.push(`(title ILIKE '%${query}%')`);
+  }
+
+  if (chunks.length === 0) return '';
+
+  if (include === true) {
+    return 'WHERE' + chunks.join(' AND ');
+  } else {
+    return 'WHERE NOT (' + chunks.join(' OR ') + ')';
+  }
+};
+
+const getOrderSQL = ({ sort, direction }) => {
+  let col = sort === 'default' ? 'id' : sort;
+  return `ORDER BY ${col} ${direction}`;
+};
+
 /******************************
  * Returns an abbreviated list of recipes
  * Used for rendering many recipe cards
  ******************************/
-module.exports.getRecipes = async ({ page = 1, count = 50, sort = 'id', direction = 'asc' }) => {
-  if (!['id', 'likes', 'price'].includes(sort) || !['asc', 'desc'].includes(direction)) {
-    return { message: 'invalid parameters' };
+module.exports.getRecipes = async (params = {}) => {
+  //default values
+  const {
+    page = 1,
+    count = 10,
+    sort = 'id',
+    direction = 'desc',
+    include = true,
+    tag_ids = [],
+    ingredient_ids = [],
+    query = '',
+  } = params;
+
+  //validate query params
+  const errors = getQueryParamErrors({
+    page,
+    count,
+    sort,
+    direction,
+    include,
+    tag_ids,
+    ingredient_ids,
+    query,
+  });
+
+  if (Object.values(errors.errors).length > 0) {
+    return errors;
   }
 
-  const offset = (page - 1) * count;
-  const end = page * count;
-
-  let where = `1=1`;
-  let order = `${sort} ${direction} OFFSET ${offset} LIMIT ${count}`;
+  const WHERE = getWhereSQL({ include, tag_ids, ingredient_ids, query });
+  const ORDER = getOrderSQL({ sort, direction });
+  const OFFSET = (page - 1) * count;
 
   const SQL = `
-    SELECT ${ABBREVIATED_COLUMNS} FROM recipes WHERE ${where} ORDER BY ${order};
-    SELECT count(1) from recipes;
+    SELECT ${ABBREVIATED_COLUMNS} FROM recipes ${WHERE} ${ORDER} OFFSET ${OFFSET} LIMIT ${count};
+    SELECT count(1) from recipes ${WHERE};
   `;
 
+  console.log(SQL);
+
   let data = await pool.query(SQL);
-  return {
+  let out = {
     page: parseInt(page),
     count: parseInt(count),
-    sort: sort === 'id' ? 'default' : sort,
+    sort: sort,
     direction: direction,
+    query: query,
+    tag_ids: tag_ids,
+    ingredient_ids: ingredient_ids,
     totalRows: parseInt(data[1].rows[0].count),
     queryRows: data[0].rowCount,
     rows: data[0].rows,
   };
+
+  return out;
 };
 
 module.exports.getRecipe = async ({ id = 2 }) => {
@@ -44,209 +144,4 @@ module.exports.getRecipe = async ({ id = 2 }) => {
   `;
   let data = await pool.query(SQL);
   return data.rowCount === 0 ? { message: 'not found' } : data.rows[0];
-};
-
-/*********************************************
- * getRecipesByIngredients
- ********************************************* */
-
-module.exports.getRecipesByIngredients = async ({
-  ids,
-  page = 1,
-  count = 10,
-  sort = 'id',
-  direction = 'asc',
-}) => {
-  if (ids.length < 1) {
-    return { message: 'must include at least 1 ingredient' };
-  }
-  if (!['id', 'likes', 'price'].includes(sort) || !['asc', 'desc'].includes(direction)) {
-    return { message: 'invalid parameters' };
-  }
-
-  const offset = (page - 1) * count;
-  const end = page * count;
-
-  const withstatement = `
-  WITH temp AS (
-    SELECT recipe_id
-    FROM recipes_ingredients
-    WHERE ingredient_id in(${ids.join(',')})
-    GROUP BY recipe_id
-    having count(distinct ingredient_id) = ${ids.length}
-  )`;
-
-  const order = `${sort} ${direction} OFFSET ${offset} LIMIT ${count}`;
-
-  const SQL = `
-    ${withstatement} SELECT ${ABBREVIATED_COLUMNS} FROM recipes
-    WHERE id in(SELECT recipe_id FROM temp) ORDER BY ${order};
-    ${withstatement} SELECT count(1) from temp;
-  `;
-
-  let data = await pool.query(SQL);
-
-  return {
-    page: parseInt(page),
-    count: parseInt(count),
-    ids: ids.map((x) => parseInt(x)),
-    sort: sort === 'id' ? 'default' : sort,
-    direction: direction,
-    totalRows: parseInt(data[1].rows[0].count),
-    queryRows: data[0].rowCount,
-    rows: data[0].rows,
-  };
-};
-
-/*********************************************
- * getRecipesByTags
- ********************************************* */
-
-module.exports.getRecipesByTags = async ({
-  ids,
-  page = 1,
-  count = 10,
-  sort = 'id',
-  direction = 'asc',
-}) => {
-  if (ids.length < 1) {
-    return { message: 'must include at least 1 tag' };
-  }
-  if (!['id', 'likes', 'price'].includes(sort) || !['asc', 'desc'].includes(direction)) {
-    return { message: 'invalid parameters' };
-  }
-
-  const offset = (page - 1) * count;
-  const end = page * count;
-
-  const withstatement = `
-  WITH temp AS (
-    SELECT recipe_id
-    FROM recipes_tags
-    WHERE tag_id in(${ids.join(',')})
-    GROUP BY recipe_id
-    having count(distinct tag_id) = ${ids.length}
-  )`;
-
-  const order = `${sort} ${direction} OFFSET ${offset} LIMIT ${count}`;
-
-  const SQL = `
-    ${withstatement} SELECT ${ABBREVIATED_COLUMNS} FROM recipes
-    WHERE id in(SELECT recipe_id FROM temp) ORDER BY ${order};
-    ${withstatement} SELECT count(1) from temp;
-  `;
-
-  let data = await pool.query(SQL);
-
-  return {
-    page: parseInt(page),
-    count: parseInt(count),
-    ids: ids.map((x) => parseInt(x)),
-    sort: sort === 'id' ? 'default' : sort,
-    direction: direction,
-    totalRows: parseInt(data[1].rows[0].count),
-    queryRows: data[0].rowCount,
-    rows: data[0].rows,
-  };
-};
-
-/*********************************************
- * filterRecipesByIngredients
- ********************************************* */
-
-module.exports.filterRecipesByIngredients = async ({
-  ids,
-  page = 1,
-  count = 10,
-  sort = 'id',
-  direction = 'asc',
-}) => {
-  if (ids.length < 1) {
-    return { message: 'must include at least 1 ingredient' };
-  }
-  if (!['id', 'likes', 'price'].includes(sort) || !['asc', 'desc'].includes(direction)) {
-    return { message: 'invalid parameters' };
-  }
-
-  const offset = (page - 1) * count;
-  const end = page * count;
-
-  const withstatement = `
-  WITH temp AS (
-    SELECT distinct recipe_id
-    FROM recipes_ingredients
-    WHERE ingredient_id in(${ids.join(',')})
-  )`;
-
-  const order = `${sort} ${direction} OFFSET ${offset} LIMIT ${count}`;
-
-  const SQL = `
-    ${withstatement} SELECT ${ABBREVIATED_COLUMNS} FROM recipes
-    WHERE id not in(SELECT recipe_id FROM temp) ORDER BY ${order};
-    ${withstatement} SELECT count(1) from recipes
-    WHERE id not in (SELECT recipe_id FROM temp);
-  `;
-
-  let data = await pool.query(SQL);
-
-  return {
-    page: parseInt(page),
-    count: parseInt(count),
-    ids: ids.map((x) => parseInt(x)),
-    sort: sort === 'id' ? 'default' : sort,
-    direction: direction,
-    totalRows: parseInt(data[1].rows[0].count),
-    queryRows: data[0].rowCount,
-    rows: data[0].rows,
-  };
-};
-/*********************************************
- * getRecipesByTags
- ********************************************* */
-
-module.exports.filterRecipesByTags = async ({
-  ids,
-  page = 1,
-  count = 10,
-  sort = 'id',
-  direction = 'asc',
-}) => {
-  if (ids.length < 1) {
-    return { message: 'must include at least 1 tag' };
-  }
-  if (!['id', 'likes', 'price'].includes(sort) || !['asc', 'desc'].includes(direction)) {
-    return { message: 'invalid parameters' };
-  }
-
-  const offset = (page - 1) * count;
-  const end = page * count;
-
-  const withstatement = `
-  WITH temp AS (
-    SELECT distinct recipe_id
-    FROM recipes_tags
-    WHERE tag_id in(${ids.join(',')})
-  )`;
-
-  const order = `${sort} ${direction} OFFSET ${offset} LIMIT ${count}`;
-
-  const SQL = `
-    ${withstatement} SELECT ${ABBREVIATED_COLUMNS} FROM recipes
-    WHERE id not in(SELECT recipe_id FROM temp) ORDER BY ${order};
-    ${withstatement} SELECT count(1) FROM recipes
-    WHERE id not in(SELECT recipe_id FROM temp);
-  `;
-
-  let data = await pool.query(SQL);
-
-  return {
-    page: parseInt(page),
-    count: parseInt(count),
-    ids: ids.map((x) => parseInt(x)),
-    sort: sort === 'id' ? 'default' : sort,
-    direction: direction,
-    totalRows: parseInt(data[1].rows[0].count),
-    queryRows: data[0].rowCount,
-    rows: data[0].rows,
-  };
 };
--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get('/recipes', async (req, res) => {
   try {
     const page = req.query.page || 1;
     const count = req.query.count || 10;
-    const sort = req.query.sort ? req.query.sort.toLowerCase() : 'id';
+    const sort = req.query.sort || 'default';
     const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
     let data = await getRecipes({ page, count, sort, direction });
     res.json(data);
@@ -65,10 +65,10 @@ app.get('/search/ingredients', async (req, res) => {
   try {
     const page = parseInt(req.query.page || 1);
     const count = parseInt(req.query.count || 10);
-    const ids = req.query.ids ? req.query.ids.split(',') : [];
-    const sort = req.query.sort ? req.query.sort.toLowerCase() : 'id';
+    const ingredient_ids = (req.query.ids ? req.query.ids.split(',') : []).map((x) => parseInt(x));
+    const sort = req.query.sort || 'default';
     const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
-    let data = await getRecipesByIngredients({ ids, page, count, sort, direction });
+    let data = await getRecipes({ page, count, sort, direction, ingredient_ids });
     res.json(data);
   } catch (err) {
     res.status(500).send({ message: 'error processing request', error: err });
@@ -79,10 +79,29 @@ app.get('/search/tags', async (req, res) => {
   try {
     const page = parseInt(req.query.page || 1);
     const count = parseInt(req.query.count || 10);
-    const ids = req.query.ids ? req.query.ids.split(',') : [];
-    const sort = req.query.sort ? req.query.sort.toLowerCase() : 'id';
+    const tag_ids = (req.query.ids ? req.query.ids.split(',') : []).map((x) => parseInt(x));
+    const sort = req.query.sort || 'default';
     const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
-    let data = await getRecipesByTags({ ids, page, count, sort, direction });
+    let data = await getRecipes({ page, count, sort, direction, tag_ids });
+    res.json(data);
+  } catch (err) {
+    res.status(500).send({ message: 'error processing request', error: err });
+  }
+});
+
+app.get('/search', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page || 1);
+    const count = parseInt(req.query.count || 10);
+    const tag_ids = (req.query.tag_ids ? req.query.tag_ids.split(',') : []).map((x) => parseInt(x));
+    const ingredient_ids = (
+      req.query.ingredient_ids ? req.query.ingredient_ids.split(',') : []
+    ).map((x) => parseInt(x));
+    const query = req.query.query || '';
+    const sort = req.query.sort || 'default';
+    const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
+
+    let data = await getRecipes({ page, count, tag_ids, ingredient_ids, query, sort, direction });
     res.json(data);
   } catch (err) {
     res.status(500).send({ message: 'error processing request', error: err });
@@ -93,10 +112,11 @@ app.get('/filter/ingredients', async (req, res) => {
   try {
     const page = parseInt(req.query.page || 1);
     const count = parseInt(req.query.count || 10);
-    const ids = req.query.ids ? req.query.ids.split(',') : [];
-    const sort = req.query.sort ? req.query.sort.toLowerCase() : 'id';
+    const ingredient_ids = (req.query.ids ? req.query.ids.split(',') : []).map((x) => parseInt(x));
+    const sort = req.query.sort || 'default';
     const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
-    let data = await filterRecipesByIngredients({ ids, page, count, sort, direction });
+    const include = false;
+    let data = await getRecipes({ page, count, sort, direction, ingredient_ids, include });
     res.json(data);
   } catch (err) {
     res.status(500).send({ message: 'error processing request', error: err });
@@ -107,10 +127,39 @@ app.get('/filter/tags', async (req, res) => {
   try {
     const page = parseInt(req.query.page || 1);
     const count = parseInt(req.query.count || 10);
-    const ids = req.query.ids ? req.query.ids.split(',') : [];
-    const sort = req.query.sort ? req.query.sort.toLowerCase() : 'id';
+    const tag_ids = (req.query.ids ? req.query.ids.split(',') : []).map((x) => parseInt(x));
+    const sort = req.query.sort || 'default';
     const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
-    let data = await filterRecipesByTags({ ids, page, count, sort, direction });
+    const include = false;
+    let data = await getRecipes({ page, count, sort, direction, tag_ids, include });
+    res.json(data);
+  } catch (err) {
+    res.status(500).send({ message: 'error processing request', error: err });
+  }
+});
+
+app.get('/filter', async (req, res) => {
+  try {
+    const page = parseInt(req.query.page || 1);
+    const count = parseInt(req.query.count || 10);
+    const tag_ids = (req.query.tag_ids ? req.query.tag_ids.split(',') : []).map((x) => parseInt(x));
+    const ingredient_ids = (
+      req.query.ingredient_ids ? req.query.ingredient_ids.split(',') : []
+    ).map((x) => parseInt(x));
+    const query = req.query.query || '';
+    const sort = req.query.sort || 'default';
+    const direction = req.query.direction ? req.query.direction.toLowerCase() : 'asc';
+    const include = false;
+    let data = await getRecipes({
+      page,
+      count,
+      tag_ids,
+      ingredient_ids,
+      query,
+      sort,
+      direction,
+      include,
+    });
     res.json(data);
   } catch (err) {
     res.status(500).send({ message: 'error processing request', error: err });
