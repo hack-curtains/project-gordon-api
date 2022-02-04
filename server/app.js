205e324694922f87f7c59f0e990e584315ea7fd1
Ian McGahren <fullstackeidolon@gmail.com>
2022-02-04 09:49:38 -0500

    Tackling cookie issues

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(
   sessions({
     secret: 'secretkeythatwouldbehiddeninproductionenvironment',
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    cookie: { maxAge: 1000 * 60 * 60 * 24 , secure: false},
     resave: false,
   })
 );
